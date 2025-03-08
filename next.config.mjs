ceae9f9bedbe8d31da7385280ad4d65cd5e6c0f1
Lencho123 <lincholechisa42@gmail.com>
2025-03-08 20:27:19 +0300

    c

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: "/Kaneh-s--Lencho-s--portifolio",
-    assetPrefix: "/Kaneh-s--Lencho-s--portifolio/",
-  };
-  
-  export default nextConfig;
-  
+  output: "export",
+  basePath: "/Kaneh-s--Lencho-s--portifolio",
+  assetPrefix: "/Kaneh-s--Lencho-s--portifolio/",
+  images: {
+    unoptimized: true, // Fixes image loading issues
+  },
+};
+
+export default nextConfig;
